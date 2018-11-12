51079dd1d8ad239da9f2cca0494136da9ca8d2cd
Eric Defore <eric.defore@gmail.com>
2018-11-12 09:51:50 -0500

    Remove Locations Loop from Contact Us Page

--- page-templates/contact-us.php
+++ page-templates/contact-us.php
@@ -29,49 +29,6 @@ while ( have_posts() ) : the_post(); ?>
 			
 	</section>
 
-	<?php 
-
-		global $post;
-
-		$locations_query = new WP_Query( array(
-			'post_type' => 'facility',
-			'posts_per_page' => -1,
-		) );
-
-		if ( $locations_query->have_posts() ) : ?>
-
-			<section id="locations" class="row">
-				
-				<div class="small-12 columns row expanded locations-container queue-on-scroll fade-in">
-				
-					<?php while( $locations_query->have_posts() ) : $locations_query->the_post(); ?>
-
-						<article <?php post_class( array( 'small-12', 'medium-3', 'columns', 'queued-item' ) ); ?> aria-labelledby="post-<?php the_ID(); ?>" style="background-image: url('<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full', false )[0]; ?>');">
-							
-							<a href="<?php the_permalink(); ?>">
-								
-								<div class="color-overlay"></div>
-							
-								<h2 id="post-<?php the_ID(); ?>"><?php the_title(); ?></h2>
-								
-							</a>
-							
-						</article>
-
-					<?php endwhile; ?>
-					
-				</div>
-
-			</section>
-
-		<?php 
-
-			wp_reset_postdata();
-
-		endif; 
-
-	?>
-
 	<section id="map" class="row">
 		
 		<div class="small-12 columns">
