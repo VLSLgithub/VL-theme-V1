1ac6014959d097ee84e597fabdf2abbaefc60036
Eric Defore <eric.defore@gmail.com>
2018-11-14 12:14:35 -0500

    Maybe support iPhone X notch...?

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 <html class="no-js" <?php language_attributes(); ?> >
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 		<?php wp_head(); ?>
 	</head>
 	
