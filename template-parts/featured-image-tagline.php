149411910aa7c2d1bd97e5fd72b4a3e12902ead6
Eric Defore <eric.defore@gmail.com>
2018-12-10 09:55:20 -0500

    Closes #27

--- template-parts/featured-image-tagline.php
+++ template-parts/featured-image-tagline.php
@@ -5,8 +5,9 @@
 		<div class="marketing">
 
 			<div class="small-12 columns image" data-interchange="[<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full', false )[0]; ?>, small], [<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full', false )[0]; ?>, medium], [<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full', false )[0]; ?>, large], [<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full', false )[0]; ?>, xlarge]">
-				<div class="color-overlay"></div>
 			</div>
+			
+			<div class="color-overlay"></div>
 
 			<div class="small-12 columns tagline">
 				<div class="row">
