7229c517bc2989113a40cdb187327aa0793fdd2d
Eric Defore <eric.defore@gmail.com>
2018-12-12 13:33:31 -0500

    Fix swirl border on a couple more templates

--- search.php
+++ search.php
@@ -8,38 +8,40 @@
 
 get_header(); ?>
 
-<div class="main-wrap swirl-border">
-	<main id="search-results" class="main-content">
+<div class="swirl-border">
+	<div class="main-wrap">
+		<main id="search-results" class="main-content">
 
-	<header>
-	    <h1 class="entry-title"><?php _e( 'Search Results for', 'vibrant-life-theme' ); ?> "<?php echo get_search_query(); ?>"</h1>
-	</header>
+		<header>
+			<h1 class="entry-title"><?php _e( 'Search Results for', 'vibrant-life-theme' ); ?> "<?php echo get_search_query(); ?>"</h1>
+		</header>
 
-	<?php if ( have_posts() ) : ?>
+		<?php if ( have_posts() ) : ?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-		<?php endwhile; ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+			<?php endwhile; ?>
 
-		<?php else : ?>
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+			<?php else : ?>
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-	<?php endif; ?>
+		<?php endif; ?>
 
-	<?php
-	if ( function_exists( 'foundationpress_pagination' ) ) :
-		foundationpress_pagination();
-	elseif ( is_paged() ) :
-	?>
-		<nav id="post-nav">
-			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'vibrant-life-theme' ) ); ?></div>
-			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'vibrant-life-theme' ) ); ?></div>
-		</nav>
-	<?php endif; ?>
-	
-	</main>
-<?php get_sidebar(); ?>
+		<?php
+		if ( function_exists( 'foundationpress_pagination' ) ) :
+			foundationpress_pagination();
+		elseif ( is_paged() ) :
+		?>
+			<nav id="post-nav">
+				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'vibrant-life-theme' ) ); ?></div>
+				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'vibrant-life-theme' ) ); ?></div>
+			</nav>
+		<?php endif; ?>
 
+		</main>
+	<?php get_sidebar(); ?>
+
+	</div>
 </div>
 
 <?php get_footer();
