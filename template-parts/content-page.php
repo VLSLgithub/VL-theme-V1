eaae3770c080f291a5f8ce9fa7098ebc7eb4181d
Eric Defore <eric.defore@gmail.com>
2018-12-12 12:54:35 -0500

    Do not show Page Title if there is a featured image

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,9 +10,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header>
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header>
+	<?php if ( ! has_post_thumbnail() ) : ?>
+		<header>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</header>
+	<?php endif; ?>
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php edit_post_link( __( '(Edit)', 'vibrant-life-theme' ), '<span class="edit-link">', '</span>' ); ?>
