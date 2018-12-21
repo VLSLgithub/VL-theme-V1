1f7d26324c72c5f52bdb760f3903ad6fcdb542eb
Eric Defore <eric.defore@gmail.com>
2018-12-21 10:16:55 -0500

    Closes #57

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -12290,20 +12290,34 @@ function resizeIframe(iFrame) {
 
 (function ($) {
 
+	function openScheduleAVisitModal() {
+
+		var $modal = $('#schedule-a-visit-modal');
+
+		$modal.foundation('open');
+
+		// Ensure we're looking at the top of the Modal
+		$modal.closest('.reveal-overlay').scrollTop(0);
+	}
+
 	$(document).ready(function () {
 
+		// Menu Items
 		$(document).on('click touch', '.menu-item.open-modal-schedule-a-visit > a', function (event) {
 
 			event.preventDefault();
 
-			var $modal = $('#schedule-a-visit-modal');
-
-			$modal.foundation('open');
-
-			// Ensure we're looking at the top of the Modal
-			$modal.closest('.reveal-overlay').scrollTop(0);
+			openScheduleAVisitModal();
 
 			$(this).parent().removeClass('clicked');
+		});
+
+		// Buttons
+		$(document).on('click touch', 'a.open-modal-schedule-a-visit', function (event) {
+
+			event.preventDefault();
+
+			openScheduleAVisitModal();
 		});
 	});
 })(jQuery);
