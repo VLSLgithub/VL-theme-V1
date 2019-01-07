4b2d899276c9060e70591d1e4ab1f1c202f01720
Eric Defore <eric.defore@gmail.com>
2019-01-07 15:43:04 -0500

    Routing Schedule a Visit Form Emails

--- library/gravity-forms.php
+++ library/gravity-forms.php
@@ -13,8 +13,7 @@ add_filter( 'gform_pre_render_2', 'vibrant_life_populate_schedule_a_visit', 10, 
  * @param		array   $form         The current form to be filtered
  * @param		boolean $ajax         Is AJAX enabled
  * @param		array   $field_values An array of dynamic population parameter keys with their corresponding values to be populated
- *                                                                                                                   
- * @access		public
+ *                     
  * @since		1.0.1
  * @return		array   Modified Form
  */
@@ -57,12 +56,16 @@ function vibrant_life_populate_schedule_a_visit( $form, $ajax, $field_values ) {
 		
 		while ( $locations->have_posts() ) : $locations->the_post();
 		
-			$field->choices[] = array(
-				'text' => rbm_cpts_get_field( 'short_name' ),
-				'value' => rbm_cpts_get_field( 'short_name' ),
-				'price' => '', // May as well send all the data
-				'isSelected' => ( $location_id && rbm_cpts_get_field( 'short_name', $location_id ) == rbm_cpts_get_field( 'short_name' ) ) ? true : false,
-			);
+			if ( function_exists( 'rbm_cpts_get_field' ) ) {
+		
+				$field->choices[] = array(
+					'text' => rbm_cpts_get_field( 'short_name' ),
+					'value' => rbm_cpts_get_field( 'short_name' ),
+					'price' => '', // May as well send all the data
+					'isSelected' => ( $location_id && rbm_cpts_get_field( 'short_name', $location_id ) == rbm_cpts_get_field( 'short_name' ) ) ? true : false,
+				);
+				
+			}
 		
 		endwhile;
 		
@@ -71,5 +74,49 @@ function vibrant_life_populate_schedule_a_visit( $form, $ajax, $field_values ) {
 	}
 	
 	return $form;
+	
+}
+
+add_filter( 'gform_notification_2', 'vibrant_life_route_schedule_a_visit_emails', 10, 3 );
+
+/**
+ * Send Email Notifications to the correct person based on the selected Location
+ * 
+ * @param		array $notification The Current Notification to be filtered
+ * @param		array $form         The form that was submitted
+ * @param		array $entry        The entry data that was submitted
+ * 
+ * @since		{{VERSION}}
+ * @return		array The filtered Notification
+ */
+function vibrant_life_route_schedule_a_visit_emails( $notification, $form, $entry ) {
+	
+	if ( ! function_exists( 'rbm_cpts_get_field' ) ) return $notification;
+	
+	if ( $notification['to'] !== 'do.not.change@fake.com' ) return $notification;
+	
+	if ( ! isset( $entry[3] ) ) return $notification; // Nothing for Field #3
+	
+	$location = new WP_Query( array(
+		'post_type' => 'facility',
+		'posts_per_page' => 1,
+		'fields' => 'ids',
+		'meta_query' => array(
+			'relationship' => 'AND',
+			array(
+				'key' => 'rbm_cpts_short_name',
+				'value' => $entry[3],
+				'compare' => '=',
+			),
+		),
+	) );
+	
+	if ( ! $location->have_posts() ) return $notification;
+	
+	$location_id = $location->posts[0];
+	
+	$notification['to'] = rbm_cpts_get_field( 'form_email', $location_id );
+	
+	return $notification;
 	
 }
